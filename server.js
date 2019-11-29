a61a7d3aa666db018b53ac25943647acce4e5aed
Leopoldo Manfio <manfio.leopoldo@gmail.com>
2019-11-29 07:04:38 -0200

    fix directoriesname erros on express use of static files

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-
 const express = require('express');
+const path = require('path');
 const app = express();
 
 // Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + '/dist'));
-
+app.use(express.static(__dirname + `/dist/browser`));
+const index = path.join(__dirname+'/dist/browser/index.html');
+app.get('/*', (req, res) => {
+    res.sendFile(index);
+});
+console.log(index);
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
